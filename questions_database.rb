9e2f36a54810a1989f77f8e79196adc78a31e739
sean <midruss@yahoo.com>
2022-01-05 14:30:40 -0500

    Started class for question table

--- questions_database.rb
+++ questions_database.rb
@@ -10,3 +10,22 @@ class QuestionsDatabase < SQLite3::Database
     self.results_as_hash = true
   end
 end
+
+class Question 
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id
+
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @users_id = options['user_id']
+  end
+end
