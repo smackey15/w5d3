91023d6a3567aad908001f018540efdb888b41e4
sean <midruss@yahoo.com>
2022-01-05 11:33:00 -0500

    Finished setting up tables

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
@@ -19,4 +21,26 @@ CREATE TABLE question_follows (
   questions_id INTEGER NOT NULL,
 
   FOREIGN KEY (users_id) REFERENCES users(id) 
+  FOREIGN KEY (questions_id) REFERENCES questions(id) 
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  questions_id INTEGER NOT NULL,
+  reply_id INTEGER,
+  users_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
+  FOREIGN KEY (reply_id) REFERENCES replies(id)
+  FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  users_id INTEGER NOT NULL,
+  questions_id INTEGER NOT NULL,
+
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
+  FOREIGN KEY (users_id) REFERENCES users(id)
 );
